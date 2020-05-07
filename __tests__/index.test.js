aa79b8897b7917c4800e1ca5382c6549faaabe9d
wafuwafu13 <mariobaske@i.softbank.jp>
2020-05-07 18:26:52 +0900

    compatible with multiple

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,23 +3,6 @@ import RandomInteger from '../index';
 const ri = new RandomInteger();
 
 expect.extend({
-    toBeWithinRange(received, floor, ceiling) {
-      const pass = received >= floor && received <= ceiling;
-      if (pass) {
-        return {
-          message: () =>
-            `expected ${received} not to be within range ${floor} - ${ceiling}`,
-          pass: true,
-        };
-      } else {
-        return {
-          message: () =>
-            `expected ${received} to be within range ${floor} - ${ceiling}`,
-          pass: false,
-        };
-      }
-    },
-
     toBeInteger(received) {
       if (Number.isInteger(received)) {
         return {
@@ -37,16 +20,22 @@ expect.extend({
     },
 });
 
-test('should return 1 to 10 integer if the value "1 10" is passed',  () => {
-    expect(ri.create(1, 10)).toBeWithinRange(1, 10)
-    expect(ri.create(1, 10)).toBeInteger()
+test('should return 1 to 10 integer if the value "1 10 10" is passed',  () => {
+    expect(Math.max(...ri.create(1, 10, 10))).toBeLessThanOrEqual(10)
+    expect(Math.min(...ri.create(1, 10, 10))).toBeGreaterThanOrEqual(1)
+    expect(ri.create(1, 10, 1)[0]).toBeInteger()
 })
 
-test('should return -10 to 0 integer if the value "-10 0" is passed',  () => {
-    expect(ri.create(-10, 0)).toBeWithinRange(-10, 0)
-    expect(ri.create(-10, 0)).toBeInteger()
+test('should return -10 to 0 integer if the value "-10 0 10" is passed',  () => {
+    expect(Math.max(...ri.create(-10, 1, 10))).toBeLessThanOrEqual(1)
+    expect(Math.min(...ri.create(-10, 1, 10))).toBeGreaterThanOrEqual(-10)
+    expect(ri.create(-10, 0, 10)[0]).toBeInteger()
 })
 
-test('should return 1 if the value "1 1" is passed',  () => {
-    expect(ri.create(1, 1)).toBe(1)
+test('should return 1 if the value "1 1 10" is passed',  () => {
+    expect(ri.create(1, 1, 1)[0]).toBe(1)
+})
+
+test('should return array of length 100 if the value "1 10 10" is passed', () => {
+    expect(ri.create(1, 10, 100).length).toBe(100)
 })
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function RandomInterger() {
-    this.create = function(min, max){
-        return Math.floor(Math.random() * (max + 1 - min)) + min
+    this.create = function(min, max, count){
+        let randomIntegerList = []
+        for(let i = 0; i < count; i++){
+            randomIntegerList.push(Math.floor(Math.random() * (max + 1 - min)) + min)
+        }
+        return randomIntegerList
     }
 }
 
