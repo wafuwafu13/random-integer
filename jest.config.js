88a697a9ef83fa8fdd60683d58dd39d684dc4643
wafuwafu13 <mariobaske@i.softbank.jp>
2020-05-07 17:28:54 +0900

    delete comment

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
     transform: {
         '^.+\\.js$'  : '<rootDir>/node_modules/babel-jest',
     },
-    moduleFileExtensions: ['js'] // テスト対象の拡張子を列挙する
+    moduleFileExtensions: ['js']
 }
